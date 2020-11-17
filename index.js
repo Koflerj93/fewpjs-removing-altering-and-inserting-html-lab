f191c02bba8836b9feab3437e97784d3cd81987f
Koflerj93 <koflerj93@gmail.com>
2020-11-16 21:42:58 -0500

    commit tree 077cdf615fcd213cf83871cbaa6a53e47c4983c9

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let name = 'Joe'
+let main = document.querySelector('#main')
+main.remove();
+
+let newHeader = document.querySelector('#victory')
+
+newHeader.innerHTML = `${name} is the champion`
